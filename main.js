e87617f8ef7cb547ac344ee4a5cee5c89740fd69
meBy <anisimovstepan163@gmail.com>
2024-02-23 16:20:24 +0700

    обновление 2.3

--- main.js
+++ main.js
@@ -1,25 +1,34 @@
 let count = 0;
 let button = document.getElementById("click-me");
 let counter = document.getElementById("counter");
-
 let body = document.querySelector(`body`);
 
 button.addEventListener(`click`, function () {
     count++;
+
     if (count % 10 == 0) {
-        counter.style.color = '#123';
+        counter.style.color = 'red';
         counter.style.fontSize = 32 + 'px';
     } else {
         counter.style.color = '#fff';
         counter.style.fontSize = 20 + 'px';
     }
+
     if (count % 50 == 0) {
-        body.style.background = 'blue';
+        body.style.background = '#000';
     } else {
         body.style.background = 'linear-gradient(rgb(6, 17, 36), rgb(11, 63, 68))';
     }
-    if (count == 100) {
-        alert('Поздравляю!');
-    }
+
+    counter.innerHTML = count;
+    saveToLocalStorage();
+})
+
+function saveToLocalStorage() {
+    localStorage.setItem('count', JSON.stringify(count));
+}
+
+window.addEventListener('load', () => {
+    count = localStorage.getItem('count');
     counter.innerHTML = count;
 })
