c195fc9b2ec9c50b4d1b335a62f62c46a16f8b6f
Foltinium <lixenses@gmail.com>
2024-02-24 12:54:44 +0700

    Обновление 2.6:     изменены все файлы

--- main.js
+++ main.js
@@ -15,7 +15,7 @@ button.addEventListener(`click`, function () {
     }
 
     if (count % 50 == 0) {
-        body.style.background = '#000';
+        body.style.background = 'linear-gradient(rgb(11, 63, 68), rgb(6, 17, 36))';
     } else {
         body.style.background = 'linear-gradient(rgb(6, 17, 36), rgb(11, 63, 68))';
     }
@@ -31,4 +31,24 @@ function saveToLocalStorage() {
 window.addEventListener('load', () => {
     count = localStorage.getItem('count');
     counter.innerHTML = count;
+
+    if (count % 10 == 0) {
+        counter.style.color = 'red';
+        counter.style.fontSize = 32 + 'px';
+    } else {
+        counter.style.color = '#fff';
+        counter.style.fontSize = 20 + 'px';
+    }
+
+    if (count % 50 == 0) {
+        body.style.background = 'linear-gradient(rgb(11, 63, 68), rgb(6, 17, 36))';
+    } else {
+        body.style.background = 'linear-gradient(rgb(6, 17, 36), rgb(11, 63, 68))';
+    }
+})
+
+document.addEventListener(`keydown`, function (evt) {
+    if (evt.key == "Enter" || evt.key == " ") {
+        evt.preventDefault();
+    }
 })
